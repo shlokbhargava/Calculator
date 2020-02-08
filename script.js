bdb802e33a55f5edb3a1732321e3adcc5df6b10d
shlokbhargava <shlokchamp88@gmail.com>
2020-02-09 02:15:28 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,82 @@
+var keypad = document.getElementsByClassName('keypad');
+var display = document.getElementById('display-area');
+var operand1 = null;
+var operand2 = null;
+var operator = null;
+var flag = 0;
+
+
+for(var i = 0; i<keypad.length; i++){
+    keypad[i].addEventListener('click', function(){
+
+        if(flag === 1){
+            display.innerText = '';
+            flag = 0;
+        }
+        var value = this.getAttribute('data-value');
+        
+        if(value == '/' || value == '*' || value == '-' || value == '+'){
+            //when an operator is clicked
+
+            operand1 = display.textContent;
+            operator = value;
+            display.innerText = value;
+            flag = 1;
+
+        }
+
+        else if(value == 'reset'){
+            //when clear button is clicked
+
+            display.innerText = '';
+        
+        }
+
+        else if(value == '='){
+            //when = is clicked
+
+            operand2 = display.textContent;
+            display.innerText = '';
+
+            if(operator == '/'){
+                if(operand2 == '0'){
+                    display.innerText = "Error";
+                }  
+                else{
+                    var result = eval("operand1 / operand2");
+                    display.innerText = result;
+                }
+            }
+            else if(operator == '+'){
+                var result = eval("operand1 + operand2")
+                display.innerText = result;
+            }
+            else if(operator == '-'){
+                var result = eval("operand1 - operand2");
+                display.innerText = result;
+            }
+            else if(operator == '*'){
+                var result = eval("operand1 * operand2");
+                display.innerText = result;
+            }
+        
+        }
+
+        // else if(value == '%'){
+        //     var result = operand1 / 100;
+        //     display.innerText = result; 
+        // }
+
+        else if(value == '+/_'){
+            display.innerText = -(display.textContent);
+        }
+
+        else{
+            //when operand is entered
+
+            display.innerText += value;
+        
+        }
+
+    });
+}
